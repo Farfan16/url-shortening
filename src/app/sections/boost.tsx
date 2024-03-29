2e51d566082961b5170ff0687ae5d7f2c8060084
Farrel Fanny <farrelexamples@gmail.com>
2024-03-29 15:39:11 +0700

    boost sections

---
+++ src/app/sections/boost.tsx
@@ -0,0 +1,12 @@
+import Button from "../components/Button";
+
+const Boost = () => {
+  return (
+    <section className="flex flex-col justify-center items-center gap-4 text-center w-full h-full px-6 py-20 bg-Boost-Mobile bg-Dark-Violet bg-no-repeat bg-cover bg-center bg-fixed">
+      <h2 className="font-bold text-white text-2xl">Boost your links today</h2>
+      <Button />
+    </section>
+  );
+};
+
+export default Boost;
