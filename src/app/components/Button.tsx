7525c2aa8253485ae2bf6aa2ee5e7871d2c7f0b3
Farrel Fanny <farrelexamples@gmail.com>
2024-03-29 15:38:54 +0700

    button component

---
+++ src/app/components/Button.tsx
@@ -0,0 +1,9 @@
+const Button = () => {
+  return (
+    <button className="bg-Cyan text-white font-semibold rounded-full px-8 py-3 inline-block">
+      Get Started
+    </button>
+  );
+};
+
+export default Button;
